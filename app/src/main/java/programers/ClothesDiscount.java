9498e227a72198b188c677dc8290d81e4111f07f
Seeker2250 <a01044777016@gmail.com>
2024-07-30 21:35:45 +0900

    programmers https://school.programmers.co.kr/learn/courses/30/lessons/120818

---
+++ app/src/main/java/programers/ClothesDiscount.java
@@ -0,0 +1,24 @@
+package programers;
+
+public class ClothesDiscount {
+    public static void main(String[] args) {
+       solution(100010); 
+    }
+    public static void solution(int price) {
+        int answer = 0;
+        double a = price;
+        if ((300000 > a) && a >= 100000) {
+          a = a - a / 20;
+          price = (int) Math.floor(a);
+    
+        } else if ((500000 > price) && price >= 300000) {
+          a = a - a / 10;
+          price = (int) Math.floor(a);
+        } else if (a >= 500000) {
+          a = (int) (a - a / 5);
+          price = (int) Math.floor(a);
+        }
+    
+        System.out.println(price);
+      }
+}
